716d5ffd6bb0a6734ad4ee7f4c3e945ce5074ab6
Multiverse88 <ainanihj@gmail.com>
2022-06-09 05:58:41 +0700

    final commit on 'main;

--- kuliah/tubes1/index.php
+++ kuliah/tubes1/index.php
@@ -68,6 +68,7 @@ if(isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="img/logo 1.ico">
     <title>Kaki bukit.</title>
 </head>
 <body>
