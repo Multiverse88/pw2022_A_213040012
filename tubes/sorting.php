fb600571d106ed8902f48092316db08e59b8e980
Multiverse88 <ainanihj@gmail.com>
2022-06-11 11:27:11 +0700

    commit on 'main'

--- tubes/sorting.php
+++ tubes/sorting.php
@@ -33,14 +33,14 @@ if (isset($_POST["id_turun"])) {
     <div class="container-urutan">
         <div class="col d-inline-flex">
          <div class="input-group me-3">
-             <button class="btn btn dropdown-toggle" style="background-color:#f5d1ad ;" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-sort-down-alt"></i>&nbsp;</button>
+             <button class="btn btn dropdown-toggle" style="background-color:#f5d1ad;" type="button" data-bs-toggle="dropdown" aria-expanded="false"><img src="img/sort-up.png" width="25px" alt=""><i class="bi bi-sort-down-alt"></i>&nbsp;</button>
                 <ul class="dropdown-menu" style="background-color:#f5d1ad ;">
                     <li><button class="dropdown-item" name="harga_naik" type="submit">Harga</button></li>
                     <li><button class="dropdown-item" name="id_naik" type="submit">Tanggal di tambahkan</button></li>
                 </ul>
          </div>
          <div class="input-group">
-             <button class="btn btn-outline dropdown-toggle" style="background-color:#f5d1ad;" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-sort-down"></i>&nbsp;</button>
+             <button class="btn btn-outline dropdown-toggle" style="background-color:#f5d1ad;" type="button" data-bs-toggle="dropdown" aria-expanded="false"><img src="img/sort-down.png" width="25px" alt=""><i class="bi bi-sort-down"></i>&nbsp;</button>
                 <ul class="dropdown-menu" style="background-color:#f5d1ad ;">
                     <li><button class="dropdown-item" name="harga_turun" type="submit">Harga</button></li>
                     <li><button class="dropdown-item" name="id_turun" type="submit">Tanggal di tambahkan</button></li>
